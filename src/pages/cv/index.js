d3bb1fe8a57f88fc8db5ce6b42b7b1947cb155eb
Joshua Levy <joshua.levy676@gmail.com>
2023-05-21 18:14:44 -0500

    Adding some properly justified text to describe download

--- src/pages/cv/index.js
+++ src/pages/cv/index.js
@@ -16,17 +16,24 @@ export default function CV() {
                     </h1>
                 </div>
 
-                <div>
-                    <DownloadButton
-                        linkTarget = "/"
-                        newTabOpt = {true}
-                    />
+                <div className = "flex flex-row justify-start items-center my-2 space-x-4">
+                    <div className = "text-lg">
+                        Last updated: May 21st, 2023
+                    </div>
+                    <div>
+                        <DownloadButton
+                            linkTarget = "/"
+                            newTabOpt = {true}
+                        />
+                    </div>
                 </div>
-                <object data = "/JoshuaLevy_CV.pdf" type = "application/pdf" width = "100%" height = "700px" className = "my-10 lg:scale-100 md:scale-0 sm:scale-0">
-                    <p>
-                        Unable to load .PDF
-                    </p>
-                </object>
+                <div className = "my-10 lg:scale-100 md:scale-0 sm:scale-0 lg:h-auto md:h-0 sm:h-0">
+                    <object data = "/JoshuaLevy_CV.pdf" type = "application/pdf" width = "100%" height = "700px">
+                        <p>
+                            Unable to load .PDF
+                        </p>
+                    </object>
+                </div>
 
             </div>
         </Layout>
