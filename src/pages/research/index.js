6a8b86f0d2f8a1d51505045183753b6c1104e954
Joshua Levy <joshua.levy676@gmail.com>
2023-05-21 21:41:49 -0500

    Setting up the basics of the Research page

--- src/pages/research/index.js
+++ src/pages/research/index.js
@@ -1,7 +1,23 @@
+import Layout from "../../components/layout.js";
+import DownloadButton from "../../components/downloadButton.js";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets : ["latin"] })
+
 export default function Research() {
     return(
-        <h1 className = "">
-            Research
-        </h1>
+        <Layout>
+            <div className = "flex flex-col w-full">
+                <div className = "my-5">
+                    <h1 className = {`${inter.className} font-bold text-3xl`}>
+                        Research
+                    </h1>
+                </div>
+            </div>
+
+            <div>
+                Test
+            </div>
+        </Layout>
     )
 }
