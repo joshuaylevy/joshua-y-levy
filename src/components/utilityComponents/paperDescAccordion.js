5bdd22e109260626d8d1a2dbab4df38d737dd388
Joshua Levy <joshua.levy676@gmail.com>
2023-05-27 22:47:44 -0500

    Adding some proper spacing and download btn to accordion

--- src/components/utilityComponents/paperDescAccordion.js
+++ src/components/utilityComponents/paperDescAccordion.js
@@ -2,26 +2,28 @@ import { Inter } from 'next/font/google';
 import { useState } from 'react';
 import Link from 'next/link';
 import SwivelChevronButton from './swivelAccordionButton';
+import DownloadButton from './downloadButton';
 
 const inter = Inter({ subsets : ["latin"] })
 
 
-export default function PaperDescriptionAccordion({ title, year, authorsList, abstract }){
+export default function PaperDescriptionAccordion({ title, year, authorsList, abstract, downloadPath, downloadTabOpt }){
+
     const [isActive, setIsActive] = useState(false);
     return(
         <div className = "flex flex-col justify-between border-gray-300 border-b mb-4">
-            <div className = "flex flex-row justify-between items-end pb-5">
-                <div>
+            <div className = "grid grid-cols-12 justify-items-stretch items-end pb-3">
+                <div className = "col-span-9 justify-self-start">
                     <h3 className = {`${inter.className} font-semibold text-2xl`}>
                         {title}
                     </h3>
                 </div>
-                <div>
+                <div className = "col-span-2 justify-self-end">
                     <h4 className = {`${inter.className} font-light text-lg`}>
                         {year}
                     </h4>
                 </div>
-                <div>
+                <div className = "col-span-1 justify-self-end">
                     <button onClick={() => setIsActive(!isActive)} className = {`${isActive && "rotate-90 duration-200"}`}>
                         <SwivelChevronButton
                             size = {20}
@@ -41,14 +43,27 @@ export default function PaperDescriptionAccordion({ title, year, authorsList, ab
                     )
                 })}
             </div>
-            {/* {isActive &&  */}
-                <div className = {`${isActive ? "max-h-40" : "max-h-0"} ${inter.className} text-justify overflow-hidden transition-[max-height] duration-200`}>
-                    <p>
-                        {abstract}
-                    </p>
+            <div className = {`${isActive ? "max-h-60" : "max-h-0"} ${inter.className} overflow-hidden transition-[max-height]  duration-200 `}>
+                <div className='flex flex-col space-y-3'>
+                    <div className = "">
+                        <p className = "text-justify">
+                            {abstract}
+                        </p>
+                    </div>
+                    <div className = "flex flow-row justify-items-start space-x-4 overflow-hidden py-5">
+                        <DownloadButton
+                            linkTarget = {downloadPath}
+                            newTabOpt = {downloadTabOpt}
+                        />
+                    </div>
+
                 </div>
-            {/* } */}
+
+
+            </div>
             
+            
+
         </div>
     )
 
--- src/pages/research/index.js
+++ src/pages/research/index.js
@@ -12,13 +12,17 @@ let examplePapers = [
         title : "Working paper title 1: evidence from test",
         year : 2023,
         authors : ["Joshua Levy", "test"],
-        abstract : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis quaerat iure quos dolorum accusantium ducimus in illum vero commodi pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque quidem maiores doloremque est numquam praesentium eos voluptatem amet! Repudiandae, mollitia id reprehenderit a ab odit!"
+        abstract : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis quaerat iure quos dolorum accusantium ducimus in illum vero commodi pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque quidem maiores doloremque est numquam praesentium eos voluptatem amet! Repudiandae, mollitia id reprehenderit a ab odit!",
+        downloadPath : "/",
+        downloadTabOpt : true
     },
     {
         title : "Second working paper title: theory and practice",
         year : 2023,
         authors : ["Joshua Levy", "test"],
-        abstract : "orem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis quaerat iure quos dolorum accusantium ducimus in illum vero commodi pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque quidem maiores doloremque est numquam praesentium eos voluptatem amet! Repudiandae, mollitia id reprehenderit a ab odit!"
+        abstract : "orem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis quaerat iure quos dolorum accusantium ducimus in illum vero commodi pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque quidem maiores doloremque est numquam praesentium eos voluptatem amet! Repudiandae, mollitia id reprehenderit a ab odit!",
+        downloadPath : "/",
+        downloadTabOpt : true
     }
 ]
 
@@ -44,6 +48,8 @@ export default function Research() {
                             year = {paper.year}
                             authorsList = {paper.authors}
                             abstract = {paper.abstract}
+                            downloadPath = {paper.downloadPath}
+                            downloadTabOpt = {paper.downloadTabOpt}
                         />
                     )
                 })}
