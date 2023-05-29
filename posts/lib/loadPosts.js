1a3df5634a1099d58a808afd1f755374302d6d31
Joshua Levy <joshua.levy676@gmail.com>
2023-05-29 16:34:59 -0500

    Blog index page improvements
    Now the index page reads in all of the blog post metadata instead of just using a slug.
    Some early attempts at styling this.
    Maybe it needs some better styling  in the future additional features for filtering by date/tag/query

--- posts/lib/loadPosts.js
+++ posts/lib/loadPosts.js
@@ -1,13 +1,36 @@
+import matter from 'gray-matter';
+import { format } from 'date-fns';
 import fs from 'fs';
 
-export async function getPostSlugs() {
-    const folder = "posts/";
-    const files = fs.readdirSync(folder);
-    const markdownPosts = files.filter((file) => file.endsWith("mdx"));
-    const slugs = markdownPosts.map((file) => file.replace(".mdx", ""));
-    return slugs
-}
+// export async function getPostSlugs() {
+//     const folder = "posts/";
+//     const files = fs.readdirSync(folder);
+//     const markdownPosts = files.filter((file) => file.endsWith("mdx"));
+//     const slugs = markdownPosts.map((file) => file.replace(".mdx", ""));
+//     return slugs
+// };
 
-export function getPostsSlugs() {
+
+export async function getPostsMetaData() {
+    const folder = "posts/"
+    const files = fs.readdirSync(folder)
+    const markdownPosts = files.filter((file) => file.endsWith("mdx"));
     
+    return markdownPosts.map((post) => {
+        const path = folder + post
+        const slug = post.replace(".mdx", "")
+        const postFile = fs.readFileSync(path, 'utf-8')
+        const grayMatter = matter(postFile)
+        const metadata = grayMatter.data
+
+        return {
+            title : metadata.title,
+            author : metadata.author,
+            preview : metadata.preview,
+            // date : metadata.publishedDate,
+            date : format(metadata.publishedDate, "MMMM d, yyyy"),
+            slug : slug
+        }
+    })
+
 }
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,36 +1,52 @@
 import Layout from "../../components/layout.js";
+import Link from "next/link.js";
 import { Inter } from "next/font/google";
-import { getPostSlugs } from "/posts/lib/loadPosts.js";
+import { getPostsMetaData } from "/posts/lib/loadPosts.js";
 
 const inter = Inter({ subsets : ["latin"] })
 
 
 
 export async function getStaticProps() {
-    const slugs = await getPostSlugs();
+    const postsMetadata = await getPostsMetaData();
     return {
-        props: { slugs }
+        props: { postsMetadata }
       };
   }
 
 
-export default function Blog({ slugs }) {
+export default function Blog({ postsMetadata }) {
     return (
         <Layout>
-            <div className = "flex flex-col w-full my-16">
+            <div className = "flex flex-col w-full mt-16">
                 <div className = "my-10">
                     <h1 className = {`${inter.className} font-bold text-4xl`}>
                         Thoughts
                     </h1>
                 </div>
             </div>
-            {slugs.map((slug) => {
-                return(
-                    <div key = {slug}>
-                        {slug}
-                    </div>
-                )
-            })}
+            <div className = "flex flex-col" >
+                {postsMetadata.map((post) => {
+                    return(
+                        <div key = {post.slug} className = "my-5">
+                            <Link
+                                href = {`/blog/${post.slug}`}
+                                target = ""
+                                className = {` ${inter.className} font-bold text-2xl hover:text-blue-500 ease-in duration-100`}
+                            >
+                                {post.title}
+                            
+                            </Link>
+                            <div>
+                                {`${post.author}  |  ${post.date}`}
+                            </div>
+                            <div>
+                                {post.preview}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
 
 
         </Layout>
