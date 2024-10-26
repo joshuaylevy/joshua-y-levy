f0a4fe563d0b03ac5382428d650ab44af96ef152
Joshua Levy <joshua.levy676@gmail.com>
2024-10-26 14:54:55 -0700

    Modifying research section
    Research page now lists papers as imported from a separate "db" file imported from another file for clean up.
    
    Adding the COI paper

--- src/pages/research/index.js
+++ src/pages/research/index.js
@@ -1,43 +1,14 @@
 import Layout from "@/components/layout.js";
 import PaperDescriptionAccordion from "@/components/utilityComponents/paperDescAccordion.js";
 import { Inter } from "next/font/google";
-
+import { wp_list } from "./working_papers_db.js";
 
 const inter = Inter({ subsets : ["latin"] })
 
 
-let examplePapers = [
-    {
-        title : "Working paper title 1: evidence from test",
-        year : 2023,
-        authors : [
-            {authorName : "Joshua Levy", authorWebsiteLink : "/", authorWebsiteLinkNewTabOpt : false},
-            {authorName : "Filippo Lancieri", authorWebsiteLink : "/", authorWebsiteLinkNewTabOpt : true},
-        ],
-        abstract : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis quaerat iure quos dolorum accusantium ducimus in illum vero commodi pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque quidem maiores doloremque est numquam praesentium eos voluptatem amet! Repudiandae, mollitia id reprehenderit a ab odit!",
-        status : "Working paper",
-        pubvenue : "Journal of Imaginary Studies",
-        downloadPath : "/",
-        downloadTabOpt : true,
-        otherButtons: []
-    },
-    {
-        title : "Second working paper title: theory and practice",
-        year : 2023,
-        authors : [
-            {authorName : "Joshua Levy", authorWebsiteLink : "/", authorWebsiteLinkNewTabOpt : false},
-            {authorName : "Filippo Lancieri", authorWebsiteLink : "/", authorWebsiteLinkNewTabOpt : true},
-        ],
-        abstract : "orem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis quaerat iure quos dolorum accusantium ducimus in illum vero commodi pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque quidem maiores doloremque est numquam praesentium eos voluptatem amet! Repudiandae, mollitia id reprehenderit a ab odit!",
-        status : "Working paper",
-        pubvenue : "Journal of Imaginary Studies",
-        downloadPath : "/",
-        downloadTabOpt : false,
-        otherButtons : [
-            {buttonText : "buttonText1", buttonLink: "/", buttonNewTabOpt : false}
-        ]
-    }
-]
+// Importing databases of papers objects
+let workingPapers = wp_list;
+
 
 export default function Research() {
     return(
@@ -53,7 +24,7 @@ export default function Research() {
                         Working Papers
                     </h2>
                 </div>
-                {examplePapers.map((paper, index) => {
+                {workingPapers.map((paper, index) => {
                     return(
                         <PaperDescriptionAccordion
                             key = {index}
---
+++ src/pages/research/working_papers_db.js
@@ -0,0 +1,40 @@
+export const wp_list = [
+    {
+        title : "The Conflict-of-Interest Discount in the Marketplace of Ideas",
+        year : 2024,
+        authors : [
+            {authorName : "John M. Barrios", authorWebsiteLink : "https://www.johnmbarrios.com/", authorWebsiteLinkNewTabOpt : true},
+            {authorName : "Filippo Lancieri", authorWebsiteLink : "https://www.law.georgetown.edu/faculty/filippo-lancieri/", authorWebsiteLinkNewTabOpt : true},
+            {authorName : "Joshua Levy", authorWebsiteLink : "/", authorWebsiteLinkNewTabOpt : false},
+            {authorName : "Shashank Singh", authorWebsiteLink : "https://shashanksinghnain.github.io/", authorWebsiteLinkNewTabOpt : true},
+            {authorName : "Tommaso M. Valletti", authorWebsiteLink : "https://profiles.imperial.ac.uk/t.valletti", authorWebsiteLinkNewTabOpt : true},
+            {authorName : "Luigi Zingales", authorWebsiteLink : "https://faculty.chicagobooth.edu/luigi-zingales", authorWebsiteLinkNewTabOpt : true},
+
+        ],
+        abstract : "We conduct a survey of economists and a representative sample of Americans to infer the reduction in the perceived value of a paper when its authors have conflicts of interest (CoI), i.e., they have financial, professional, or ideological stakes in the outcome of the results. On average, a CoI decreases trust in the conclusions of an economics paper by 30%. This reduction in trust reflects a combination of the frequency of conflicted papers and the bias of papers when they are conflicted. To isolate the second term, we introduce a key construct: the CoI Discount, which measures the reduction in the value of a conflicted paper relative to a non-conflicted one. We show that, on average, conflicted papers are worth less than half of non-conflicted ones, though this effect varies significantly depending on the nature of the conflict. The discount is more pronounced when the conflict involves the interest of a private rather than a public entity. Restricted data access also leads to a substantial discount. We validate our survey based estimates by comparing them to actual biases observed in conflicted papers within the economics and medical literature.",
+        status : "",
+        pubvenue : "SSRN",
+        downloadPath : "/research_pdfs/COI-10-24-SSRN.pdf",
+        downloadTabOpt : true,
+        otherButtons: [
+            {buttonText : "SSRN", buttonLink: "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4979205", buttonNewTabOpt : true}
+        ]
+    },
+    {
+        title : "Second working paper title: theory and practice",
+        year : 2023,
+        authors : [
+            {authorName : "Joshua Levy", authorWebsiteLink : "/", authorWebsiteLinkNewTabOpt : false},
+            {authorName : "Filippo Lancieri", authorWebsiteLink : "/", authorWebsiteLinkNewTabOpt : true},
+        ],
+        abstract : "orem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis quaerat iure quos dolorum accusantium ducimus in illum vero commodi pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque quidem maiores doloremque est numquam praesentium eos voluptatem amet! Repudiandae, mollitia id reprehenderit a ab odit!",
+        status : "Working paper",
+        pubvenue : "Journal of Imaginary Studies",
+        downloadPath : "/",
+        downloadTabOpt : false,
+        otherButtons : [
+            {buttonText : "buttonText1", buttonLink: "/", buttonNewTabOpt : false}
+        ]
+    }
+
+]
